ee39a5fe4fcff30fe06a38852ffd7eed0d53da24
inponomarev <iponomarev@mail.ru>
2018-05-28 01:28:08 +0300

    more verbose output

--- src/ru/curs/ratcheting/utils.groovy
+++ src/ru/curs/ratcheting/utils.groovy
@@ -1,13 +1,16 @@
 package ru.curs.ratcheting
+
 boolean compareWarnings(before, after) {
     boolean result = true
     before.forEach { module, info ->
         info.forEach { checker, warnings ->
+            printf "${module}.${checker}: ${warnings}->${after[module][checker]}"
             if (after[module][checker] > warnings) {
-                println "${module}.${checker}: ${warnings}->${after[module][checker]}"
+                println("<-------------!!!!")
                 result = false
-            }
+            } else println()
         }
+
     }
     return result
 }
