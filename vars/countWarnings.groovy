75c373b01f4ef2fee42050b76ac8b20b21b68e58
inponomarev <iponomarev@mail.ru>
2018-05-30 13:32:58 +0300

    added countWarnings

--- vars/countWarnings.groovy
+++ vars/countWarnings.groovy
@@ -1,1 +1,16 @@
+def call(List modules){
+    def warningsMap = [:]
+    for (module in modules){
+        def text = new File(module + "/target/checkstyle-result.xml").text
+        def checkstyle = new XmlSlurper().parseText(text)
+        def csCount = checkstyle.file.error.size()
 
+
+        text = new File(module + "/target/findbugsXml.xml").text
+        def bugCollection = new XmlSlurper().parseText(text)
+        def fbCount =a bugCollection.BugInstance.size()
+
+        warningsMap.put(module, [checkstyle: csCount, findbugs: fbCount])
+    }
+    return warningsMap
+}
