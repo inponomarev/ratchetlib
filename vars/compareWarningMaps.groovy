0c7c7b5a4c46cca24d7d0d7f98a4f16b8f8f4cf8
inponomarev <iponomarev@mail.ru>
2018-09-28 01:21:03 +0300

    improved logic for cases when modules/checkers appear/disappear

--- vars/compareWarningMaps.groovy
+++ vars/compareWarningMaps.groovy
@@ -1,26 +1,33 @@
 def call(Map before, Map after) {
     boolean result = true
-    def msg = ''
-    before.each { module, info ->
-        info.each { checker, warnings ->
-            def newWarnings = after[module][checker]
-            if (newWarnings > warnings) {
+    msg = ''
+
+    Set modules = before.keySet() + after.keySet()
+
+    modules.each { module ->
+        oldInfo = before[module] ?: [:]
+        newInfo = after[module] ?: [:]
+
+        Set checkers = oldInfo.keySet() + newInfo.keySet()
+        checkers.each {checker ->
+            oldWarnings = oldInfo[checker] ?: 0
+            newWarnings = newInfo[checker] ?: 0
+
+            if (newWarnings > oldWarnings) {
                 msg <<= "\n+ "
                 result = false
-            } else if (newWarnings < warnings) {
+            } else if (newWarnings < oldWarnings) {
                 msg <<= "\n- "
             } else
                 msg <<= "\n= "
-            msg <<= "${module}.${checker}: ${warnings}->${after[module][checker]}"
+            msg <<= "${module}.${checker}: ${oldWarnings}->${newWarnings}"
         }
     }
+
     echo "${msg}"
-    if (result){
+    if (result) {
         echo "Ratcheting: no new issues found"
     } else {
         error "Ratcheting failed, see messages above."
     }
 }
-
-
-
